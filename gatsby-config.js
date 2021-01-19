6720bcfd9df4d7970f26b4baff596ff5456de0d5
Karyn Vo <vkaryn@gmail.com>
2021-01-18 21:14:24 -0800

    added sitemap data

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,11 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
+    {
+      siteMetadata: {
+        siteUrl: "myurl"
+      }
+    },
     "gatsby-plugin-offline",
     {
       resolve: "gatsby-plugin-manifest",
